837b7d092eac222ee7f3a4b8ea948824fb55ee5d
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2024-03-29 09:30:34 +0530

    add the dataset files and download scripts

---
+++ datasets/kathbath/download.sh
@@ -0,0 +1,8 @@
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/clean/train_audio.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/clean/valid_audio.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/clean/testkn_audio.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/clean/testunk_audio.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/clean/transcripts_n2w.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/noisy/testkn_audio.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/noisy/testunk_audio.tar
+wget --no-check-certificate https://indic-asr-public.objectstore.e2enetworks.net/indic-superb/kathbath/noisy/transcripts_n2w.tar
---
+++ datasets/voxelceleb1h/download.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate https://mm.kaist.ac.kr/datasets/voxceleb/data/vox1_dev_txt.zip
+wget --no-check-certificate https://mm.kaist.ac.kr/datasets/voxceleb/data/vox1_test_txt.zip
