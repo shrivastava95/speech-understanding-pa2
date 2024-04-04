4f1796cba448c2693909589fef6ae3e355cb9f9c
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2024-04-04 19:19:19 +0530

    add voxceleb1h test subset from Rishabh TA

--- datasets/voxelceleb1h/download.sh
+++
@@ -1,4 +0,0 @@
-wget --no-check-certificate https://mm.kaist.ac.kr/datasets/voxceleb/data/vox1_dev_txt.zip
-wget --no-check-certificate https://mm.kaist.ac.kr/datasets/voxceleb/data/vox1_test_txt.zip
-wget --no-check-certificate https://mm.kaist.ac.kr/datasets/voxceleb/meta/list_test_hard.txt
-wget --no-check-certificate https://mm.kaist.ac.kr/datasets/voxceleb/meta/list_test_hard2.txt
